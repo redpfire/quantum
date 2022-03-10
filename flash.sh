61ca300e8719016cea29e62291e202a4f69b0d82
redpfire <redpixafire@gmail.com>
2022-03-10 23:58:41 +0100

    feat: Combos and qwerty opted out

--- flash.sh
+++ flash.sh
@@ -2,8 +2,8 @@
 
 mkdir -p qmk_firmware/keyboards/3w6/keymaps/aika
 
-cp config.h qmk_firmware/keyboards/3w6/keymaps/aika/
-qmk json2c keymap.json -o qmk_firmware/keyboards/3w6/keymaps/aika/keymap.c
+cp {config.h,keymap.c,rules.mk} qmk_firmware/keyboards/3w6/keymaps/aika/
+#qmk json2c keymap.json -o qmk_firmware/keyboards/3w6/keymaps/aika/keymap.c
 
 pushd qmk_firmware/keyboards/3w6/keymaps/aika
 qmk flash -kb 3w6/rev3 -km aika
