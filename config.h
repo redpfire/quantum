0c7d028283de24319a62627e32a61a34f86a8dd8
redpfire <redpixafire@gmail.com>
2022-03-13 20:04:38 +0100

    feat: Make home row mods more usable

--- config.h
+++ config.h
@@ -1,9 +1,11 @@
 // Configure the global tapping term (default: 200ms)
-#define TAPPING_TERM 170
+#define TAPPING_TERM 165
 
 // Prevent normal rollover on alphas from accidentally triggering mods.
 #define IGNORE_MOD_TAP_INTERRUPT
 
 // Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
-//#define TAPPING_FORCE_HOLD
-#define COMBO_COUNT 2
+#define TAPPING_FORCE_HOLD
+
+#define PERMISSIVE_HOLD
+#define COMBO_COUNT 4
