ebb0e2aa7a153405bba8d334eeb73a0acef9a1d3
redpfire <redpixafire@gmail.com>
2022-05-20 00:43:48 +0200

    feat: Add capslock (sic!)

--- keymap.c
+++ keymap.c
@@ -14,12 +14,14 @@ const uint16_t PROGMEM numbers_combo[] = {KC_H, KC_COMMA, COMBO_END};
 const uint16_t PROGMEM numbers_combo_sym[] = {KC_AMPR, KC_CIRC, COMBO_END};
 const uint16_t PROGMEM vim_combo[] = {KC_H, KC_COMMA, KC_DOT, COMBO_END};
 const uint16_t PROGMEM vim_combo_sym[] = {KC_AMPR, KC_CIRC, KC_TILD, COMBO_END};
+const uint16_t PROGMEM keylock_combo[] = {KC_X, KC_C, KC_D, COMBO_END};
 
 combo_t key_combos[] = {
     [NUM_LAYER] = COMBO(numbers_combo, TO(3)),
     [NUM_LAYER_SYM] = COMBO(numbers_combo_sym, TO(3)),
     [VIM_LAYER] = COMBO(vim_combo_sym, TO(4)),
     [VIM_LAYER_SYM] = COMBO(vim_combo, TO(4)),
+    [KEYLOCK_CAPS] = COMBO(keylock_combo, KC_CAPS),
 };
 
 /* THIS FILE WAS GENERATED!
--- rules.mk
+++ rules.mk
@@ -1,1 +1,2 @@
 COMBO_ENABLE = yes
+KEY_LOCK_ENABLE = yes
